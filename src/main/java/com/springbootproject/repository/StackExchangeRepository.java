0b71efef04a021b88b04868ed98ff0875a05f20c
Vitaliy030 <60977465+Vitaliy030@users.noreply.github.com>
2020-12-23 17:16:14 +0200

    Fix mistakes

--- src/main/java/com/springbootproject/repository/StackExchangeRepository.java
+++ src/main/java/com/springbootproject/repository/StackExchangeRepository.java
@@ -1,10 +1,8 @@
 package com.springbootproject.repository;
 
 import com.springbootproject.entity.Sites;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,12 +11,14 @@ import java.net.URISyntaxException;
 
 @Component
 public class StackExchangeRepository {
-    HttpClient httpClient = HttpClientBuilder.create().build();
-    ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-    private RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+    @Value("${service.url}")
+    private String url;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public Sites getSites() {
-        final String url = "https://api.stackexchange.com/2.2/sites?filter=!2--Yion.6iyFFudE8CzDu";
         try {
             final Sites response = restTemplate.getForObject(new URI(url), Sites.class);
             return response;
