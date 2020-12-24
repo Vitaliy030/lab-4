b44babadce60e86465d4842832872e0df54ce0dc
Vitaliy030 <60977465+Vitaliy030@users.noreply.github.com>
2020-12-24 11:55:30 +0200

    Fix mistakes

--- src/main/java/com/springbootproject/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/springbootproject/exception/RestResponseEntityExceptionHandler.java
@@ -2,19 +2,21 @@ package com.springbootproject.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler {
-    @ExceptionHandler(value = Exception.class )
-    public ResponseEntity<Object> handleConflict(final Exception ex, final WebRequest request) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-    }
-
     @ExceptionHandler(value = ServiceException.class)
     public ResponseEntity<ServiceExceptionDTO> handleServiceException(final ServiceException ex, final WebRequest request) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ServiceExceptionDTO(ex));
     }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ServiceExceptionDTO> handleServiceException(final MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ServiceExceptionDTO(400, ex.getMessage(),null));
+    }
+
 }
